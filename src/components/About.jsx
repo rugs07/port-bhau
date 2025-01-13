ed28773fc216093f711db11b76d51de14923ebde
rugs07 <rugwed07@gmail.com>
2025-01-13 10:53:49 +0530

    changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
 
       <motion.p
         variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        className="mt-4 text-secondary text-[17px] w-full leading-[30px]"
       >
         I am a passionate developer with hands-on experience in creating
         innovative and secure applications. With a solid foundation in
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[100px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -20,8 +20,8 @@ const Hero = () => {
               className='text-[#4c4cff]'>Rugwed</span>
             </h3>
         <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I develop Fullstack <span
-              className='text-[#fffc4c]'>Interfaces</span> Web <br className=' sm:block hidden'/>
+          I develop Website <span
+              className='text-[#fffc4c]'>Interfaces</span>  <br className=' sm:block hidden'/>
           using MERN
         </p>
         </div>
