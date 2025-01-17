1cd057b8bbaf36a592098e8e93f5bb3cbde33a06
rugs07 <rugwed07@gmail.com>
2025-01-17 20:12:39 +0530

    contact page width changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -66,11 +66,11 @@ const Contact = () => {
 
   return (
     <div
-      className={`xl:mt-10 flex xl:flex-row flex-col gap-10 overflow-hidden`}
+      className={`xl:mt-10 flex xl:flex-row flex-col gap-10 overflow-hidden items-center justify-center`}
     >
       <motion.div
         variants={slideIn("left", "tween", 0.2, 1)}
-        className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
+        className='flex-[0.75] bg-black-100 w-full p-8 rounded-2xl'
       >
         <p className={styles.sectionSubText}>Get in touch</p>
         <h3 className={styles.sectionHeadText}>Contact.</h3>
