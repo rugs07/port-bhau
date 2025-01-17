f8d39d4f4a55e8088e2d10a22c1e10102ef59809
rugs07 <rugwed07@gmail.com>
2025-01-17 18:31:23 +0530

    changes name asset images

--- src/assets/index.js
+++ src/assets/index.js
@@ -38,8 +38,8 @@ import svg from './svg.png'
 import webgl from './webgl.png'
 import redux from './redux.png'
 import community from "./community.png"
-import jar from "./jar.png"
-import enterprise from "./enterprise.png"
+import jar from "./jar.PNG"
+import enterprise from "./enterprise.PNG"
 import logo1 from "./logo.jpg"
 
 export {
