1c1433a14e97afe372eb377406a02e266b81b491
rugs07 <rugwed07@gmail.com>
2025-01-18 12:57:59 +0530

    Design changes in tech and index.css

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-
 import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
+    <div className="technology-grid">
+      {technologies.map((tech, index) => (
+        <div className={`tech-card ${index < 14 ? "simple-design" : ""}`} key={tech.name}>
+          <div className="icon-wrapper">
+            <img src={tech.icon} alt={tech.name} />
+          </div>
+          <h3>{tech.name}</h3>
         </div>
       ))}
     </div>
